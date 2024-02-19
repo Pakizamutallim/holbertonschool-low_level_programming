95924bd9a8a30d48697c525a7f71a12f296575c1
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-19 05:43:16 -0800

    Write

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                                                                                  
-                                                                                                                                       
-/**                                                                                                                                 
-* main - Entry point                                                                                                               
-* Return: Always 0 (Success)                                                                                                       
-*/                                                                                                                                 
-int main(void)                                                                                                                      
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
@@ -12,4 +12,4 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
-} 
+}
