2bf33d3cd77b6c56543ac25eca3e8f65667665c8
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-22 12:44:35 +0000

    written now

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Empty point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
