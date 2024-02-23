edcd733f8482f02b95875c94bfe7732548065a45
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-23 13:20:23 +0000

    Functions, nested loops committed

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Empty point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		if (islower(c) != c)
+		{
+			return(0)
+		}
+		else
+		{
+			return(0);
+		}
+	}
+
+}
