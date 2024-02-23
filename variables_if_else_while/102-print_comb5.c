507495b6b52e837456054c20e843b0cdfd9e3bed
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-23 09:06:48 +0000

    written now

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 		for (y = x + 1; y <= 99; y++)
 		{
 			putchar((x / 10) + '0');
-			putchar((y % 10) + '0');
+			putchar((x % 10) + '0');
 			putchar(' ');
 			putchar((y / 10) + '0');
 			putchar((y % 10) + '0');
