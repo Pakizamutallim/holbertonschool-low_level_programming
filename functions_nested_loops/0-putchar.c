7f0b3d8ec776d3f24ed84289622b23b93d4cd78e
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-23 10:03:52 +0000

    Functions, nested loops committed

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Empty point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
