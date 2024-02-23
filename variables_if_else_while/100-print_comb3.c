d43296fda55c9667d22eaddbd8025aacdf22260d
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-23 07:47:40 +0000

    written now

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (x = 8 || y = 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
