acaa9ed36850f64d60e75cff6c5206001a287302
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-23 11:01:44 +0000

    Functions, nested loops committed

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Empty point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet(x);
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
