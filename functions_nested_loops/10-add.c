fead59d8685b95b385852c1b2926cdb9c8a6f5ab
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-25 15:04:36 +0000

    Functions, nested loops committed

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- * add - check lowercase.
+ * add - adds two numbers together.
  *
  * @a: The first integer to be added.
  *
- * @b: The second integer to be added.
+ * @b: The second integer to be added. 
  */
+
+
+int add(int a, int)
 
 int add(int a, int b)
 {
