7bb57e61193052387c1f50a24d6e42333b2f8fdf
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 15:51:33 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_line - check the code.
+ * @n: It is a num.
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n < 0)
+	{
+		while (a <= n)
+		{
+			-putchar('_');
+			a++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+}
