6522e148a09e03e87d30a5a3f51ea7fc6aeecea4
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 12:45:29 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_numbers - check the code.
+ * @c: - The number to be checked.
+ * Return: 1 for upper letter or 0 for otherwise.
+ */
+
+void print_numbers(void)
+{
+	int (a);
+
+	for (a = 48; a <= 57; a++)
+	{
+		return (a);
+	}
+}
