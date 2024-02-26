60bc78350d723fb909d6253c0268588492ea83ec
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 12:05:57 +0000

    more_functions_nested_loops file committed

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,12 @@
 /**
  * _isupper - check the code.
  * @c - is a letter.
- * Return: Always 0.
+ * Return: 1 for upper letter or 0 for otherwise.
  */
 
 int _isupper(int c)
 {
-	if (x>=65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
