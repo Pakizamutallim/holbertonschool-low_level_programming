b8270c3afb3d97f2ae51a52e65ed71e537f03a5a
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 12:36:48 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mul - check the code
+ * @a: - The number to be checked.
+ * @b: - The number to be checked.
+ * Return: 1 for upper letter or 0 for otherwise.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
