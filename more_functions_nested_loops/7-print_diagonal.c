c5c8c12a5a6b6f10f6a9146f1afaf1e7b8c78df8
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 16:52:32 +0000

    more_functions_nested_loops file committed

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,17 @@
 
 void print_diagonal(int n)
 {
-	int i = 0;
+	int i, x;
 
 	if (i > 0)
 	{
-		while (i > n)
+		for (i = 0; i > n; i++)
 		{
+			for (x = 0; x > i; x++)
+			{
+				_putchar(32);
+			}
 			_putchar(92);
-			_putchar(32);
 		}
 	}
 	else
