c442ac195683f10f361b72f2aa002b0ae1c5a406
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 17:28:48 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_line - function that checks for uppercase character.
+ * @size: is the integer for the paramaters of my function
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(35);
+			}
+			_putchar(35);
+		}
+	}
+	else 
+	{
+		_putchar('\n');
+	}
+}
