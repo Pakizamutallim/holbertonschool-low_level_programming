e8609cba5a3adf69d4b6d669f513b51dee55f09f
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 12:17:54 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isdigit - check the code.
+ * @c: - The number to be checked.
+ * Return: 1 for upper letter or 0 for otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
