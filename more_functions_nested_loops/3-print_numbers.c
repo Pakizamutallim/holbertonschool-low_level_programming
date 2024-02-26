01e60e9ee9e6f55d183a73d8952b9827bd15a811
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 13:17:06 +0000

    more_functions_nested_loops file committed

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,17 +4,16 @@
 
 /**
  * print_numbers - check the code.
- * @a: - The number to be checked.
- * Return: 1 for upper letter or 0 for otherwise.
+ * @a: The number to be checked.
  */
 
 void print_numbers(void)
 {
-	char a = '0';
+	int a = '0';
 
 	while (a <= '9')
 	{
-		putchar(a);
+		_putchar(a);
 		a++;
 	}
 	_putchar('\n');
