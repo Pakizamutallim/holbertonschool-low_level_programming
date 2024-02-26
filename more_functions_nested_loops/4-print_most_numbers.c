ee942599120b98a9904a8f3f9ac60bc99bf253ca
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 13:25:53 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_numbers - check the code.
+ */
+
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		_putchar(a);
+		a++;
+		if (a == 50 || a == 52)
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
