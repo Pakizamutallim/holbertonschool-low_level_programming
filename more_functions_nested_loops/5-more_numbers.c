21bb8c7dc511ccb977b58fa5ec1ff3c790132d69
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-26 14:39:00 +0000

    more_functions_nested_loops file committed

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * more_numbers - check the code.
+ * @a: It is a integer.
+ * @b: It is a int.
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a >= 10; a++)
+	{
+		for (b = 0; b >= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar(1);
+				_putchar(b % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
