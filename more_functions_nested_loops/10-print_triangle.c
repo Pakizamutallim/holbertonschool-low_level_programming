f4b3db1c01890165f44b6f9d9cf7af541bf00afe
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-27 12:21:16 +0000

    more_functions_nested_loops file committed

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,18 +11,22 @@
 
 void print_triangle(int size)
 {
-	int a, b;
+	int a, b, c;
 
 	if (size > 0)
 	{
 		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b < a; b++)
+			for (b = size; b > a; b--)
+			{
+				_putchar(32);
+			}
+			for (c = 0; c < a; c++)
 			{
 				_putchar(35);
-				_putchar('\n');
 			}
 			_putchar(35);
+			_putchar('\n');
 		}
 	}
 	else
