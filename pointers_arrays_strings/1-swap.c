1b03d60164d29e629548987626c0281a3a72afd8
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-29 10:50:48 +0000

    committed

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - updates the value of the variable
+ * *a: a pointer for first value
+ * *b: a pointer for second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
