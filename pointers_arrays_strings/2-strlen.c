c30864a284eba8f2a6bee285e6a3d2691a49eb68
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-29 11:26:17 +0000

    committed

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen- updates the value of the variable
+ * the pointer points to to 98
+ * @s: pointer to the variable to update
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	i = 0;
+
+	while (*s [i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
