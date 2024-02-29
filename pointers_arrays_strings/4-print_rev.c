fd6f7cde1d7ad5b358f4d5ab3f89bd2946d2daba
Pakizamutallim <62bq5tn@code.edu.az>
2024-02-29 12:30:17 +0000

    committed

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h
+
+/**
+ * print_rev - updates the value of the variable
+ * the pointer points to to 98
+ * @s: pointer to the variable to update
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int length = strlen(str);
+
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(str[i]);
+}
