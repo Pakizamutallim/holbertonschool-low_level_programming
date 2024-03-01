2525ba35b96323e1776c65d81112ebeefe5c7611
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-01 20:02:10 +0000

    committed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _atoi - updates the value of the variable
+ * @s: pointer to the variable to update
+ */
+
  int _atoi(char *s)
 {
 	int i;
+	int length = 0;
 
-	i = _atio(s);
-	printf("%d", i);
+	while (s[length] != 0)
+	{
+		length++;
+	}
+	for (i = 0; i < length; i++)
+	{
+		printf("%d", i);
+		_putchar('\n');
+	}
+	_putchar('\n');
 }
