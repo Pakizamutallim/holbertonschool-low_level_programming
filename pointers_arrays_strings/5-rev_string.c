92fdb468689083755a3c01d2304b0c9cfb8b3cd2
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-01 11:06:48 +0000

    committed

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - updates the value of the variable
+ * the pointer points to to 98
+ * @str: pointer to the variable to update
+ */
+
+void rev_string(char *s)
+{
+	int i, a;
+
+	int length = 0
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	i = length - 1;
+
+	for (a = 0; a < length / 2; a++)
+	{
+		tmp = s[i];
+		s[i] = s[
+	}
+}
