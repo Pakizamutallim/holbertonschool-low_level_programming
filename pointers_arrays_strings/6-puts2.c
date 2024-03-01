35fc2370b87c76cf8a3560404f5a2b7714e99286
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-01 13:12:36 +0000

    committed

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
+
+
+void puts2(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
