328b1471716d6b794d345cfb6a54e0d8f3a7f571
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-01 19:02:07 +0000

    committed

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-
-	while (a[i] < n)
+	for (i = 0; i < n; i++)
 	{
-		a[i]++;
-
-		if (a[i] == 0)
+		if (i == 0)
 		{
 			printf("%d", a[i]);
 		}
