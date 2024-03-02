67fbe90c5903f1b8c8e64422c3cae0292aa47767
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-02 17:55:34 +0000

    committed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,17 +8,38 @@
 
  int _atoi(char *s)
 {
-	int i;
+	int i = 0;
 	int length = 0;
+	int n = 0;
+	int f = 0;
+	int digit;
+	int d
 
 	while (s[length] != 0)
 	{
 		length++;
 	}
-	for (i = 0; i < length; i++)
+	while (i < length && f ==0)
 	{
-		printf("%d", i);
-		_putchar('\n');
+		if (s[i] == '-')
+		{
+			++d;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i +1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	_putchar('\n');
+
+	if (f == 0)
+		return(0);
+	return(n);
 }
