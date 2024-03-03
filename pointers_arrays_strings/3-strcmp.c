cc79c4f9a1e57cd7e5e2c0d0a3bab3e7aca62b7e
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-03 11:47:55 +0000

    committed

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - updates the value of the variable
+ * the pointer points to to 98
+ * @s1: variable to update
+ * @s2: it is a variable
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (s1 == s2)
+	{
+		if (s1 == 0)
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (s1 - s2);
+}
