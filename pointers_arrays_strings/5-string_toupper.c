2fe655cf99f96adf450799be3127726c44234de1
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-03 13:34:16 +0000

    committed

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *string_toupper - updates the value of the variable
+ * the pointer points to to 98
+ * @s: pointer to the variable to update
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
