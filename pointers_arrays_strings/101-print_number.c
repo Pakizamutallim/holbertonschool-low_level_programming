2c523d3476ce7695d3235cb347d0db0f24e174c8
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-03 15:24:11 +0000

    committed

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,4 @@
  */
 char *rot13(char *s)
 {
-	int a, b;
-	char *lets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	for (a = 0; s[i] != 0; a++)
-	{
-		for (b = 0; b < 52; b++)
-		{
-			if (s[a] == lets[b])
-				s[a] = rot[b];
-		}
-	}
-	return (s);
 }
