047648fbc354dae1db5bfe176bb88bd90dd69393
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-03 13:11:16 +0000

    committed

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - updates the value of the variable
+ * @a: the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, b, rev;
+
+	i = 0;
+	b = n - 1;
+
+	while (i < n / 2)
+	{
+		i++;
+		rev = a[i];
+		a[i] = a[b];
+		a[b--] = rev;
+	}
+
+}
