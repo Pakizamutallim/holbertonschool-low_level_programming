33f5b4eb92b986a3f45a4fd0d7b0400a16619836
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-03 07:48:42 +0000

    committed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,40 +8,32 @@
 
  int _atoi(char *s)
 {
-	int i = 0;
-	int length = 0;
-	int n = 0;
-	int f = 0;
-	int digit;
-	int d;
+	int signs = 1;
+	unsigned int sum = 0;
+	int final = 0;
 
-	while (s[length] != 0)
+	for (; *s != '\0'; s++)
 	{
-		length++;
+		if (*s == '-')
+		{
+			signs *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+			break;
 	}
-	while (i < length && f == 0)
+	while (*s >= '0' && *s <= '9')
 	{
-		if (s[i] == '-')
-		{
-			++d;
-			s[i] *= -1;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-
-			if (d % 2)
-				digit = digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i +1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		sum *= 10;
+		sum += *s - '0';
+		s++;
 	}
-
-	if (f == 0)
-		return(0);
-	return(n);
+	if (signs == -1)
+	{
+		final = -sum;
+	}
+	else
+	{
+		final = sum;
+	}
+	return (final);
 }
