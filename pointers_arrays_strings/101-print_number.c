420d832ef3e8fc031a79c6f3afa0dd69309c21c4
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-03 15:20:02 +0000

    committed

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int a, b;
+	char *lets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[i] != 0; a++)
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (s[a] == lets[b])
+				s[a] = rot[b];
+		}
+	}
+	return (s);
+}
