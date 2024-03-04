f7825418d0b21aead5a13b716c462dd28c68483e
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-04 12:43:37 +0000

    committed

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,22 +2,20 @@
 #include <stdio.h>
 
 /**
- * *cap_string - updates the value of the variable
- * the pointer points to to 98
- * @s: pointer to the variable to update
+ * *_memcpy - updates the value of the variable
+ * @dest: the pointer points to to 98
+ * @n: a number of array
+ * @src: pointer to the variable to update
  * Return: returns result.
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		for(j = 0; j <= i; j++)
-		{
-			src[i] = dest[j];
-		}
+		src[i] = dest[i];
 	}
 	return (dest);
 }
