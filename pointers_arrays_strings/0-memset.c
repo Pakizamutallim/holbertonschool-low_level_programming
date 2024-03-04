a1eda2edc00692035256d184844fe4cdbbd07b9a
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-04 12:28:30 +0000

    committed

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * *_memset - updates the value of the variable
+ * @b: the pointer points to to 98
+ * @n: a number of *s pointers
+ * @s: pointer to the variable to update
+ * Return: returns result.
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
