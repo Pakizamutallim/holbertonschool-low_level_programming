41584d9dee2fad478f809848571c6e4f21c7341c
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-05 11:37:02 +0000

    committed

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,6 +5,8 @@
  * *_strpbrk - updates the value of the variable
  * @accept: the pointer points
  * @s: pointer to the variable to update
+ *
+ * Return: returns a result.
  */
 
 char *_strpbrk(char *s, char *accept)
@@ -15,7 +17,7 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (j = 0; accept[j]; j++)
 		{
-			if ( s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				return (s + i);
 			}
