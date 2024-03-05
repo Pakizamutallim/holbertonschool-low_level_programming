f51006b00f0e9c5f7f605c72a851c871d56d3dc1
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-05 12:43:32 +0000

    committed

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -20,5 +20,5 @@ void print_diagsums(int *a, int size)
 		sum_1 = sum_1 + a[l + i];
 		sum_2 = sum_2 + a[(size * (i + 1)) - (i + 1)];
 	}
-	printf("%d", "%d", sum_1, sum_2);
+	printf("%d", "%s", sum_1, sum_2);
 }
