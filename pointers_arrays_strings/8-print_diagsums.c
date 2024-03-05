1ffc817d7ad82b4922857442af4a9884332d9035
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-05 12:41:48 +0000

    committed

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * - check the code
+ *
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	unsigned int sum_1 = 0;
+	unsigned int sum_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		int l = size * i;
+
+		sum_1 = sum_1 + a[l + i];
+		sum_2 = sum_2 + a[(size * (i + 1)) - (i + 1)];
+	}
+	printf("%d", "%d", sum_1, sum_2);
+}
