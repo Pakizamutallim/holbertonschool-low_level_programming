14e130c81c9318eaf17f8d222354c157f098cce7
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-06 13:28:19 +0000

    recursion committed

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -10,9 +10,12 @@
 
 int _strlen_recursion(char *s)
 {
+	int length = 0;
+
 	if (*s)
 	{
-		_strlen_recursion(s + 1);
-		_putchar(*s);
+		length++;
+		length += _strlen_recursion(s + 1);
 	}
+	return (length);
 }
