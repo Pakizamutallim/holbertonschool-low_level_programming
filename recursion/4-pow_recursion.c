d027269e7ba507fe13ad8c0d29b365007b755787
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-07 08:29:52 +0000

    recursion committed

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * *cap_string - updates the value of the variable
- * the pointer points to to 98
- * @s: pointer to the variable to update
+ * _pow_recursion - updates the value of the variable
+ * @x: the pointer points to to 98
+ * @y: pointer to the variable to update
  * Return: returns result.
  */
 
@@ -13,6 +13,10 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 	{
 		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
 	}
 	return ((_pow_recursion(x, y - 1) * x));
 }
