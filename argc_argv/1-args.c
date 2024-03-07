d5d7f4ffe83faec7e0a8730062b2df7bea884e31
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-07 13:13:12 +0000

    argc and argv dir committed

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,18 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-		{
-			;
-		}
-		printf("%s\n", argv[i]);
-	}
+	(void) argv;
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
