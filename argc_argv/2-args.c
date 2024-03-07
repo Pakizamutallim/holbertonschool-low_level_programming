3539f22a77f30d5b0961d8ba1fcc0de24b515332
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-07 13:28:07 +0000

    argc and argv dir committed

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - updates the value of the variable
+ * @argc:the pointer points to to 98
+ * @argv: pointer to the variable to update
+ * Return: returns result.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < *argc - 1; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
