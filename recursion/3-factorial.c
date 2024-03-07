151380b361edeabbf92f296ee9dee1c24e798c4c
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-07 07:43:26 +0000

    recursion committed

---
+++ recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial- updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ * Return: returns result.
+ */
+
+int factorial(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		return (0);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * (factorial(n - 1));
+}
