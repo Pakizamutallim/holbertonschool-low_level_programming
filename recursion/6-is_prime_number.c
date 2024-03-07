df9efc9a4de5fba84af140ba99fc89147c060ed9
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-07 11:26:36 +0000

    recursion committed

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,19 +10,26 @@
 
 int my_is_prime(int n, int a)
 {
-	if (n == 0 || n == 1)
+	if (n == a)
 	{
-		return (0);
+		return (1);
 	}
 	if (n % a == 0)
 	{
 		return (0);
 	}
+	if (n < 0)
+	{
+		return (0);
 
 	return (my_is_prime(n, a + 1));
 }
 
 int is_prime_number(int n)
 {
+	if (n == 0 || n == 1)
+	{
+		return (0);
+	}
 	return (my_is_prime(n, 2));
 }
