3698baaadf9887bf4e902bd2b85931ec0233ca04
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-07 12:28:14 +0000

    argc and argv dir committed

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - updates the value of the variable
+ * @argv: the pointer points to to 98
+ * @argc: pointer to the variable to update
+ * Return: returns result.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
