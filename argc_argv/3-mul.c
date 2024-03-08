f50beb3d5132bf03589786c4da522d379497941f
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-08 13:35:26 +0000

    argc and argv dir committed

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,10 @@ int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	int result = argv[1] * argv[2];
+	int num = atoi(*argv[2]);
+	int num2 = atoi(*argv[3]);
+
+	int result = num * num2;
 
 	printf("%d", result);
 }
