1df1851b189a7af7a7133f4abd62bbc10b645e7f
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-11 12:29:23 +0000

    committed to malloc_free

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,8 +11,27 @@
 
 char *_strdup(char *str)
 {
+	char *array = malloc(strlen(str) * sizeof(char));
+	int i, j;
+
 	if (str == NULL)
 		return (NULL);
-
-
+	else
+	{
+		for (i = 0; str[i] < strlen[str]; i++)
+		{
+			if (array == NULL)
+			{
+				return (NULL);
+			}
+			else
+			{
+				for (j = 0; array[j] < str[i]; j++)
+				{
+					array[j] = str[i];
+				}
+			}
+		}
+	}
+	return (array);
 }
