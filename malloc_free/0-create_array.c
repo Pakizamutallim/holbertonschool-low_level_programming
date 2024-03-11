bdb7106b4624500eae3b8e3ed659b49a8b366da9
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-11 10:54:04 +0000

    committed to malloc_free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,4 +12,18 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		arr[i] = c;
+	}
+	return (arr);
 }
