7c77147887335c1acfa32538090fdde9a616f12e
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-11 10:14:08 +0000

    committed to malloc_free

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+}
