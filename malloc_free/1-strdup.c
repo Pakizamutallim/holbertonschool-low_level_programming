8cdc5cda088a08d1d82d942508fb90ebbdadce50
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-11 11:52:21 +0000

    committed to malloc_free

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+
+}
