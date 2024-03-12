146ee78f1e3478a3a1771f9981a712043e66eac8
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-12 20:05:34 +0000

    committed to malloc_free

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @grid: the address of the two dimensional grid
+ * @grid: the address of the two dimensional grid
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid
+}
