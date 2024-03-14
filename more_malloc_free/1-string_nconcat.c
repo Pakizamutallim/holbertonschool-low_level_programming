46c26ecee7fa5e545368afbc7577286aa454bff0
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-14 12:36:25 +0000

    committed for more_malloc_free

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: 0.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, len1 = 0, len2 = 0;
+	char *sconcat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != 0; i++)
+	{
+		len1++;
+	}
+	for (i = 0; s2[i] != 0; i++)
+		len2++;
+
+	sconcat = malloc(sizeof(char) * (len1 + n) + 1);
+
+	if (sconcat == NULL)
+	{
+		return (NULL);
+	}
+	if (len1 <= n)
+	{
+		for (i = 0; s1[i] != 0; i++)
+			sconcat[i] = s1[i];
+		for (i = 0; s2[i] != 0; i++)
+			sconcat[len1 + i] = s2[i];
+	}
+	else
+	{
+		for (i = 0; s1[i] != 0; i++)
+			sconcat[i] = s1[i];
+		for (i = 0; i < n; i++)
+			sconcat[len1 + i] = s2[i];
+	}
+	return (sconcat);
+}
