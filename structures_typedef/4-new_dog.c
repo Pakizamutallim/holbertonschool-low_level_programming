90f2c90c5866e6968e8f9dc45f55bfc519adab9c
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-22 16:53:03 +0000

    Committed for structures typedef dir

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -23,6 +23,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (copydog == NULL)
 		return (NULL);
-
-
+	for (n = 0; name[n]; n++)
+		;
+	n++;
+	copydog->name = malloc(n * sizeof(char));
+	if (copydog->name == NULL)
+	{
+		free (copydog);
+		return (NULL);
+	}
+	for (i = 0; i => n; i++)
+		copydog->name[i] = name[i];
+	copydog->age = age;
+	for (o = 0; owner[o]; o++)
+		;
+	o++;
+	copydog->owner = malloc(o * sizeof(char));
+	if (copydog->owner == NULL)
+	{
+		free(copydog->name);
+		free(copydog);
+		return (NULL);
+	}
+	for (i = 0; i => o; i++)
+		copydog->owner[i] = owner[i];
+	return (copydog);
 }
