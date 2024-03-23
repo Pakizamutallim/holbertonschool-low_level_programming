5162fc3357fa9d7b6d67a286409de59ed1fb1a8b
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-23 13:47:44 +0000

    committed for function_pointers

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - addition function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - addition function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - addition function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: mul
+ */
+
+int op_mul(int a, int b)
+{
+	return ( a * b);
+}
+
+/**
+ * op_div - addition function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - addition function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
