df059611560d40cf6699e8df93c636fe991655d0
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-23 12:33:37 +0000

    committed for function_pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,9 +3,18 @@
 
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
+ *
+ * @name: string pointer
+ *
+ * @f: pointer
+ * Return: 0.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
+	{
+		f(name);
+	}
 }
