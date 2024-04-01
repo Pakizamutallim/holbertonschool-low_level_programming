3a27d2ca1b7a1e962ff50fe76468f6bbdf33bc76
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-01 13:26:49 +0000

    committed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -18,10 +18,11 @@ size_t print_list(const list_t *h)
 	while(h != 0)
 	{
 		if(h->str == NULL)
-			printf("[%d]", "%s", 0, (nil));
+			printf("[%d]", "%s", 0, "(nil)");
 		else
-			printf("[%d]", "%s", h->len, h->str);
+			printf("[%s]", "%s", h->len, h->str);
 		h = h->next;
 		i++;
 	}
+	return (i);
 }
