8bfacd11baa2e7d381a5b50bb00514930500002c
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-02 12:27:22 +0000

    committed

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,14 +13,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	unsigned int len = 0, i = 0;
+	unsigned int i = 0;
 
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
-	if (strdup(str) == NULL)
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
@@ -28,9 +30,10 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[i] != 0)
 	{
 		i++;
+		str++;
 	}
-	new_node->len = len;
-	new_node->str = strdup(str);
+	new_node->len = i;
+
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
