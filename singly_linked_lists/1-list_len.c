e5e9b1ef8efae5930ddd87c0022bfca13040b611
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-02 10:59:05 +0000

    committed

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h = '\0')
+	{
+		i++;
+		h = h->next;
+	}
+	return (k);
+}
