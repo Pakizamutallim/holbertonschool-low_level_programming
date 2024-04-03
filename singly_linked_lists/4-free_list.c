6a2281a823032338d8e5de7fbbaa645d7e3f13d7
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-03 14:19:43 +0000

    committed

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *freelist;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	while (*head != NULL)
+	{
+		freelist = head;
+		head = (*head)->next;
+		free(freelist);
+	}
+	return (1);
+}
