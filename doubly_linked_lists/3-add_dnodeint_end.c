a97393a67b095ce62c697e55172c930dd330e5a9
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-03 18:24:44 +0000

    committed

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,5 +23,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		new->prev = aux;
 		aux->next = new;
 	}
-	return(new);
+	else
+	{
+		*head = new;
+		new->prev = NULL;
+	}
+
+	return (new);
 }
