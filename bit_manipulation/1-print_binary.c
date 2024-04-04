f165fe094b7a30d8e932d2a7ad02119b5ee3da5e
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-04 13:23:59 +0000

    committed for bit_manipulation

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ * @n: insugned long int.
+ * Return: Always 0.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+
+	for (i = 1 << 31; i > 0; i = i / 2)
+	{
+		if (n & i)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+	}
+}
