064ead001858b88060b48e14cc744dec4e39192c
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-04 12:54:29 +0000

    committed for bit_manipulation

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int count = 0;
 	int i = 0;
 
-	if (b != NULL)
+	if (b == NULL)
 	{
-		while (b[i] == '0' || b[i] == '1')
-		{
-			count <<= 1;
-			count += b[i] - '0';
-			i++;
-		}
-	}
-	else
 		return (NULL);
+	}
+	while (b[i] == '0' || b[i] == '1')
+	{
+		count <<= 1;
+		count += b[i] - '0';
+		i++;
+	}
 	return (count);
 }
