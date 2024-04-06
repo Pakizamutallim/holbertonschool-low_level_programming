d7bf5992f834a711c582808082d2a8d8b0497e93
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-06 09:17:36 +0000

    committed for bit_manipulation

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 0;
+
+	while ((n ^ m) > 0)
+	{
+		i = i + ((n ^ m) & 1);
+		(n ^ m) >>= 1
+
+	return (i);
+}
