c9cd8e7dd309032ba6fc0b5b6d7b7d5cf467da60
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-06 09:07:11 +0000

    committed for bit_manipulation

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+
+
+/**
+ * clear_bit - a function that sets the value of a bit to 0 at a given index.
+ * @n: A pointer
+ * @index: is the index, starting from 0
+ *
+ * Return: -1 or 1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n = n & (~(1 << index));
+
+	return (1);
+}
