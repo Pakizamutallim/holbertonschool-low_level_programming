597e935757b9edc5435625eee76ca79cf7c56225
Pakizamutallim <62bq5tn@code.edu.az>
2024-04-07 16:09:18 +0000

    committer for file I/O dir

---
+++ file_io/3-cp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * copy_to - a function that copies the content of a file
+ * @file_to: a file copy to
+ * @file_from: a file for copy from
+ * Return: o
+ */
